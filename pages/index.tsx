1d1a5f8b7c33c96170bd912b1e2b59073433463d
Uche Ozoemena <codewithoz@outlook.com>
2022-11-10 08:42:23 +0100

    Style the header section

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,14 +2,14 @@ import Head from 'next/head'
 
 export default function Home() {
   return (
-    <div className="container">
+    <div>
       <Head>
         <title>Half-Baked Thoughts</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <section className='flex justify-content-end align-items-start text-3xl font-bold underline'>
+        <section className='flex justify-center py-3 2xl:py-7 bg-amber-600 text-amber-900 shadow-full shadow-amber-900 items-center text-xl lg:text-3xl 2xl:text-6xl font-bold'>
           <h1>
             Books I've read
           </h1>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        'full': '0 0px 30px 10px rgb(0 0 0 / 1)'
+      }
+    },
   },
   plugins: [],
 }
